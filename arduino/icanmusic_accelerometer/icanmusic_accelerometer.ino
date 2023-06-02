698100b825b69053bc0483df6a9ecabad3b324f1
Don Undeen <donundeen@gmail.com>
2023-06-02 12:45:10 -0400

    new changes

---
+++ arduino/icanmusic_accelerometer/icanmusic_accelerometer.ino
@@ -0,0 +1,478 @@
+//gyro code:
+// * LSM6DSOX + LIS3MDL FeatherWing : https://www.adafruit.com/product/4565
+// * ISM330DHCX + LIS3MDL FeatherWing https://www.adafruit.com/product/4569
+// * LSM6DSOX + LIS3MDL Breakout : https://www.adafruit.com/product/4517
+// * LSM6DS33 + LIS3MDL Breakout Lhttps://www.adafruit.com/product/4485
+// see here: https://learn.adafruit.com/st-9-dof-combo/arduino 
+/*
+ * Libraries to install (include dependencies)
+ * OSC
+ * Adafruit LSM6DS 
+ * Adafruit LIS3MDL
+ * AutoConnect (see https://hieromon.github.io/AutoConnect/#installation)
+ * 
+ */
+#include <Adafruit_LSM6DSOX.h>
+Adafruit_LSM6DSOX lsm6ds;
+
+#include <Adafruit_LIS3MDL.h>
+Adafruit_LIS3MDL lis3mdl;
+
+/*
+ * Required libraries to install in the arduino IDE (use the Library Manager to find and install):
+ * https://github.com/Hieromon/PageBuilder : PageBuilder
+ * https://github.com/bblanchon/ArduinoJson : ArduinoJson
+ * https://github.com/CNMAT/OSC : OSC
+ * AutoConnect: https://hieromon.github.io/AutoConnect/index.html : instructions on how to install are here: 
+ * follow the instructions under "Install the AutoConnect" if you can't just find it in the Library Manager
+ */
+
+// this is all the OSC libraries
+#include <SLIPEncodedSerial.h>
+#include <OSCData.h>
+#include <OSCBundle.h>
+#include <OSCBoards.h>
+#include <OSCTiming.h>
+#include <OSCMessage.h>
+#include <OSCMatch.h>
+
+// these the libraries for connecting to WiFi
+// based on docs here: https://hieromon.github.io/AutoConnect/gettingstarted.html 
+#include <WiFi.h>
+#include <AutoConnect.h>
+#include <WebServer.h>
+
+/* 
+ *  WIFI_MODE_ON set to true to send osc data over WIFI.
+ *  When this is true: 
+ *  -- if the arduino can't connect to wifi, it will create its own AP, named esp32_ap (pw 12345678)
+ *  -- you'll need to connect to that SSID via your phone, and use the interface that pops up on your phone 
+ *     to configure the SSID and PW of the router you want to connect to
+ *  When WIFI_MODE_ON = false, you need the arduino connected to the laptop, 
+ *  and it will send data over serial USB
+ */
+const boolean WIFI_MODE_ON = true;
+
+/* if we aren't using the auto-configuration process, 
+    and we want to hard-code the router's SSID and password here.
+    Also set HARDCODE_SSID = true
+*/
+const boolean HARDCODE_SSID = true;
+
+// remember you can't connect to 5G networks with the arduino. 
+
+const char *WIFI_SSID = "Studio314";
+const char *WIFI_PASSWORD = "!TIE2lacesWiFi";
+//const char * UDPReceiverIP = "10.0.0.164"; // ip where UDP messages are going
+const char * UDPReceiverIP = "10.102.134.110"; // ip where UDP messages are going
+
+const int UDPPort = 9002; // the UDP port that Max is listening on
+
+bool wifi_connected =false;
+
+/*
+ * Sometimes we need to delete the SSIDs that are stored in the config of the arduino.
+ * Set this value to TRUE and rerun the arduino, to remove all the stored SSIDs 
+ * (aka clear the configuration storage). 
+ * Then set it badk to false to start saving new SSID/Passwords
+ * 
+ */
+const boolean DELETE_SSIDS = false;
+
+String thisperifitid = "";
+String thisarduinomac = "";
+String thishumanname = "";
+String thisarduinoip = "";
+
+
+//create UDP instance
+WiFiUDP udp;
+
+// wifi autoconnect code
+WebServer Server;
+AutoConnect      Portal(Server);
+AutoConnectConfig  config;
+
+OSCErrorCode error;
+
+
+static boolean doConnect = false;
+
+
+void rootPage() {
+  char content[] = "Hello, world";
+  Server.send(200, "text/plain", content);
+}
+
+
+void deleteAllCredentials(void) {
+  Serial.println("deleting all stored SSID credentials");
+  AutoConnectCredential credential2;
+  boolean result;
+  
+  result = credential2.del((const char*)"GuestNet");
+  Serial.println(result);
+
+  station_config_t config2;
+  uint8_t ent = credential2.entries();
+  Serial.print("Num SSIDS: ");
+  Serial.println(ent);
+
+  while (ent--) {
+    credential2.load((int8_t)0, &config2);
+    Serial.println((const char*)&config2.ssid[0]);
+    result = credential2.del((const char*)&config2.ssid[0]);
+    Serial.println(result);
+  }
+}
+
+
+
+// sending data over OSC/UDP.
+void sendOSCUDP(float gx, float gy, float gz, int sensorVal){
+  /* egs
+   *  '/perifit/1', valueInt1, valueInt2, device.name);
+   *  28:ec:9a:14:2b:b3 l 180
+      28:ec:9a:14:2b:b3 u 1391
+   *  
+   */
+ if(WiFi.status() == WL_CONNECTED){   
+  //send hello world to server
+  char ipbuffer[20];
+  thisarduinoip.toCharArray(ipbuffer, 20);
+  OSCMessage oscmsg("/accel1/val");  
+  oscmsg.add(gx).add(gy).add(gz).add(ipbuffer).add(sensorVal);
+
+  udp.beginPacket(UDPReceiverIP, UDPPort);
+//  udp.write(buffer, msg.length()+1);
+  oscmsg.send(udp);
+  udp.endPacket();
+  oscmsg.empty();
+ }else{
+  Serial.println("not sending udp, not connected");
+ }
+
+
+}
+
+/*
+ * connecting to UDP port on laptop runnin Max
+ */
+void configUdp(){
+  if(WIFI_MODE_ON){
+    if(!wifi_connected && WiFi.status() == WL_CONNECTED){
+      Serial.println("HTTP server:" + WiFi.localIP().toString());
+      thisarduinoip = WiFi.localIP().toString();
+      Serial.println("SSID:" + WiFi.SSID());
+      wifi_connected = true;
+      udp.begin(UDPPort);
+    }
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.println("wifi not connected");
+      wifi_connected = false;
+    }
+  }
+}
+
+
+void setup() {
+//  Serial.begin(115200);
+//  Serial.begin(9600);
+  Serial.begin(38400);  
+  delay(1000);
+  Serial.println("setup");
+
+  // for incoming UDP
+//  SLIPSerial.begin(115200);
+  pinMode(21, INPUT_PULLUP);
+
+
+  pinMode(BUILTIN_LED, OUTPUT);
+
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+
+  thisarduinomac = WiFi.macAddress();
+
+  if(WIFI_MODE_ON){
+    if(!DELETE_SSIDS){
+
+      // wifi config business
+
+      if(HARDCODE_SSID){
+        Serial.println("connecting to hardcoded SSID");
+        Serial.println(WIFI_SSID);
+        Serial.println(WIFI_PASSWORD);
+        
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        while (WiFi.status() != WL_CONNECTED) {
+          // wifi status codes: https://realglitch.com/2018/07/arduino-wifi-status-codes/
+          delay(1000);
+          Serial.print(".");
+          Serial.print(WiFi.status());
+          Serial.print(WL_CONNECTED);
+        }
+      }else{
+        Server.on("/", rootPage);
+        Serial.println("done with Server.on");  
+        config.portalTimeout = 15000;  // It will time out in 15 seconds
+        Portal.config(config);
+        Portal.begin();
+      }
+    }else{
+      deleteAllCredentials();
+    }
+  }
+  
+  /*if (Portal.begin()) {
+    Serial.println();
+    Serial.println("HTTP server:" + WiFi.localIP().toString());
+
+    // setup UDP:
+      udp.begin(UDPPort);
+  }else{
+    Serial.println("not portal.begin");
+  }
+  */
+
+  // gyro/accel stuff
+  bool lsm6ds_success, lis3mdl_success;
+
+  // hardware I2C mode, can pass in address & alt Wire
+
+  lsm6ds_success = lsm6ds.begin_I2C();
+  lis3mdl_success = lis3mdl.begin_I2C();
+
+  if (!lsm6ds_success){
+    Serial.println("Failed to find LSM6DS chip");
+  }
+  if (!lis3mdl_success){
+    Serial.println("Failed to find LIS3MDL chip");
+  }
+  if (!(lsm6ds_success && lis3mdl_success)) {
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("LSM6DS and LIS3MDL Found!");
+
+  // lsm6ds.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (lsm6ds.getAccelRange()) {
+  case LSM6DS_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case LSM6DS_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case LSM6DS_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case LSM6DS_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+
+  // lsm6ds.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (lsm6ds.getAccelDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  // lsm6ds.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
+  Serial.print("Gyro range set to: ");
+  switch (lsm6ds.getGyroRange()) {
+  case LSM6DS_GYRO_RANGE_125_DPS:
+    Serial.println("125 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  case ISM330DHCX_GYRO_RANGE_4000_DPS:
+    Serial.println("4000 degrees/s");
+    break;
+  }
+  // lsm6ds.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Gyro data rate set to: ");
+  switch (lsm6ds.getGyroDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
+  // You can check the datarate by looking at the frequency of the DRDY pin
+  Serial.print("Magnetometer data rate set to: ");
+  switch (lis3mdl.getDataRate()) {
+    case LIS3MDL_DATARATE_0_625_HZ: Serial.println("0.625 Hz"); break;
+    case LIS3MDL_DATARATE_1_25_HZ: Serial.println("1.25 Hz"); break;
+    case LIS3MDL_DATARATE_2_5_HZ: Serial.println("2.5 Hz"); break;
+    case LIS3MDL_DATARATE_5_HZ: Serial.println("5 Hz"); break;
+    case LIS3MDL_DATARATE_10_HZ: Serial.println("10 Hz"); break;
+    case LIS3MDL_DATARATE_20_HZ: Serial.println("20 Hz"); break;
+    case LIS3MDL_DATARATE_40_HZ: Serial.println("40 Hz"); break;
+    case LIS3MDL_DATARATE_80_HZ: Serial.println("80 Hz"); break;
+    case LIS3MDL_DATARATE_155_HZ: Serial.println("155 Hz"); break;
+    case LIS3MDL_DATARATE_300_HZ: Serial.println("300 Hz"); break;
+    case LIS3MDL_DATARATE_560_HZ: Serial.println("560 Hz"); break;
+    case LIS3MDL_DATARATE_1000_HZ: Serial.println("1000 Hz"); break;
+  }
+
+  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
+  Serial.print("Range set to: ");
+  switch (lis3mdl.getRange()) {
+    case LIS3MDL_RANGE_4_GAUSS: Serial.println("+-4 gauss"); break;
+    case LIS3MDL_RANGE_8_GAUSS: Serial.println("+-8 gauss"); break;
+    case LIS3MDL_RANGE_12_GAUSS: Serial.println("+-12 gauss"); break;
+    case LIS3MDL_RANGE_16_GAUSS: Serial.println("+-16 gauss"); break;
+  }
+
+  lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE);
+  Serial.print("Magnetometer performance mode set to: ");
+  switch (lis3mdl.getPerformanceMode()) {
+    case LIS3MDL_LOWPOWERMODE: Serial.println("Low"); break;
+    case LIS3MDL_MEDIUMMODE: Serial.println("Medium"); break;
+    case LIS3MDL_HIGHMODE: Serial.println("High"); break;
+    case LIS3MDL_ULTRAHIGHMODE: Serial.println("Ultra-High"); break;
+  }
+
+  lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
+  Serial.print("Magnetometer operation mode set to: ");
+  // Single shot mode will complete conversion and go into power down
+  switch (lis3mdl.getOperationMode()) {
+    case LIS3MDL_CONTINUOUSMODE: Serial.println("Continuous"); break;
+    case LIS3MDL_SINGLEMODE: Serial.println("Single mode"); break;
+    case LIS3MDL_POWERDOWNMODE: Serial.println("Power-down"); break;
+  }
+
+  lis3mdl.setIntThreshold(500);
+  lis3mdl.configInterrupt(false, false, true, // enable z axis
+                          true, // polarity
+                          false, // don't latch
+                          true); // enabled!
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // this handles the wifi config business:
+
+  if(!HARDCODE_SSID){
+    Portal.handleClient();
+  }
+  configUdp();
+
+  // get button value:
+    int sensorVal = digitalRead(21);
+  //print out the value of the pushbutton
+  Serial.println(sensorVal);
+
+  // Keep in mind the pull-up  means the pushbutton's logic is inverted. It goes
+  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
+  // button's pressed, and off when it's not:
+  if (sensorVal == HIGH) {
+    digitalWrite(13, LOW);
+  } else {
+    digitalWrite(13, HIGH);
+  }
+
+sensors_event_t accel, gyro, mag, temp;
+
+  //  /* Get new normalized sensor events */
+  lsm6ds.getEvent(&accel, &gyro, &temp);
+  lis3mdl.getEvent(&mag);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x, 4);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y, 4);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z, 4);
+  Serial.println(" \tm/s^2 ");
+
+  /* Display the results (rotation is measured in rad/s) */
+  Serial.print("\t\tGyro  X: ");
+  Serial.print(gyro.gyro.x, 4);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y, 4);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z, 4);
+  Serial.println(" \tradians/s ");
+  
+  sendOSCUDP(gyro.gyro.x, gyro.gyro.y, gyro.gyro.z, sensorVal);
+
+
+  delay(10);
+}
---
+++ arduino/icanmusic_capacitive/icanmusic_capacitive.ino
@@ -0,0 +1,246 @@
+/*
+ * Libraries to install (include dependencies)
+ * OSC
+ * AutoConnect (see https://hieromon.github.io/AutoConnect/#installation)
+ * 
+ */
+
+// ESP32 Touch Test
+// Just test touch pin - Touch0 is T0 which is on GPIO 4.
+
+// using 15 - This is GPIO #15 and also an analog input A8 on ADC #2
+
+int touchPin = 15;
+
+
+/*
+ * Required libraries to install in the arduino IDE (use the Library Manager to find and install):
+ * https://github.com/Hieromon/PageBuilder : PageBuilder
+ * https://github.com/bblanchon/ArduinoJson : ArduinoJson
+ * https://github.com/CNMAT/OSC : OSC
+ * AutoConnect: https://hieromon.github.io/AutoConnect/index.html : instructions on how to install are here: 
+ * follow the instructions under "Install the AutoConnect" if you can't just find it in the Library Manager
+ */
+
+// this is all the OSC libraries
+#include <SLIPEncodedSerial.h>
+#include <OSCData.h>
+#include <OSCBundle.h>
+#include <OSCBoards.h>
+#include <OSCTiming.h>
+#include <OSCMessage.h>
+#include <OSCMatch.h>
+
+// these the libraries for connecting to WiFi
+// based on docs here: https://hieromon.github.io/AutoConnect/gettingstarted.html 
+#include <WiFi.h>
+#include <AutoConnect.h>
+#include <WebServer.h>
+
+/* 
+ *  WIFI_MODE_ON set to true to send osc data over WIFI.
+ *  When this is true: 
+ *  -- if the arduino can't connect to wifi, it will create its own AP, named esp32_ap (pw 12345678)
+ *  -- you'll need to connect to that SSID via your phone, and use the interface that pops up on your phone 
+ *     to configure the SSID and PW of the router you want to connect to
+ *  When WIFI_MODE_ON = false, you need the arduino connected to the laptop, 
+ *  and it will send data over serial USB
+ */
+const boolean WIFI_MODE_ON = true;
+
+/* if we aren't using the auto-configuration process, 
+    and we want to hard-code the router's SSID and password here.
+    Also set HARDCODE_SSID = true
+*/
+const boolean HARDCODE_SSID = true;
+
+// remember you can't connect to 5G networks with the arduino. 
+
+const char *WIFI_SSID = "Studio314";
+const char *WIFI_PASSWORD = "!TIE2lacesWiFi";
+//const char * UDPReceiverIP = "10.0.0.164"; // ip where UDP messages are going
+const char * UDPReceiverIP = "10.102.134.110"; // ip where UDP messages are going
+
+const int UDPPort = 9002; // the UDP port that Max is listening on
+
+bool wifi_connected =false;
+
+/*
+ * Sometimes we need to delete the SSIDs that are stored in the config of the arduino.
+ * Set this value to TRUE and rerun the arduino, to remove all the stored SSIDs 
+ * (aka clear the configuration storage). 
+ * Then set it badk to false to start saving new SSID/Passwords
+ * 
+ */
+const boolean DELETE_SSIDS = false;
+
+String thisperifitid = "";
+String thisarduinomac = "";
+String thishumanname = "";
+String thisarduinoip = "";
+
+
+//create UDP instance
+WiFiUDP udp;
+
+// wifi autoconnect code
+WebServer Server;
+AutoConnect      Portal(Server);
+AutoConnectConfig  config;
+
+OSCErrorCode error;
+
+
+static boolean doConnect = false;
+
+
+void rootPage() {
+  char content[] = "Hello, world";
+  Server.send(200, "text/plain", content);
+}
+
+
+void deleteAllCredentials(void) {
+  Serial.println("deleting all stored SSID credentials");
+  AutoConnectCredential credential2;
+  boolean result;
+  
+  result = credential2.del((const char*)"GuestNet");
+  Serial.println(result);
+
+  station_config_t config2;
+  uint8_t ent = credential2.entries();
+  Serial.print("Num SSIDS: ");
+  Serial.println(ent);
+
+  while (ent--) {
+    credential2.load((int8_t)0, &config2);
+    Serial.println((const char*)&config2.ssid[0]);
+    result = credential2.del((const char*)&config2.ssid[0]);
+    Serial.println(result);
+  }
+}
+
+
+
+// sending data over OSC/UDP.
+void sendOSCUDP(int touchVal){
+  /* egs
+   *  '/perifit/1', valueInt1, valueInt2, device.name);
+   *  28:ec:9a:14:2b:b3 l 180
+      28:ec:9a:14:2b:b3 u 1391
+   *  
+   */
+ if(WiFi.status() == WL_CONNECTED){   
+  //send hello world to server
+  char ipbuffer[20];
+  thisarduinoip.toCharArray(ipbuffer, 20);
+  OSCMessage oscmsg("/capacitive1/val");  
+  oscmsg.add(touchVal).add(ipbuffer);
+  Serial.print("sending data ");
+  Serial.println(touchVal);
+
+  udp.beginPacket(UDPReceiverIP, UDPPort);
+//  udp.write(buffer, msg.length()+1);
+  oscmsg.send(udp);
+  udp.endPacket();
+  oscmsg.empty();
+ }else{
+  Serial.println("not sending udp, not connected");
+ }
+
+
+}
+
+/*
+ * connecting to UDP port on laptop runnin Max
+ */
+void configUdp(){
+  if(WIFI_MODE_ON){
+    if(!wifi_connected && WiFi.status() == WL_CONNECTED){
+      Serial.println("HTTP server:" + WiFi.localIP().toString());
+      thisarduinoip = WiFi.localIP().toString();
+      Serial.println("SSID:" + WiFi.SSID());
+      wifi_connected = true;
+      udp.begin(UDPPort);
+    }
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.println("wifi not connected");
+      wifi_connected = false;
+    }
+  }
+}
+
+
+void setup() {
+//  Serial.begin(115200);
+//  Serial.begin(9600);
+  Serial.begin(38400);  
+  delay(1000);
+  Serial.println("setup");
+
+
+
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+
+  thisarduinomac = WiFi.macAddress();
+
+  if(WIFI_MODE_ON){
+    if(!DELETE_SSIDS){
+
+      // wifi config business
+
+      if(HARDCODE_SSID){
+        Serial.println("connecting to hardcoded SSID");
+        Serial.println(WIFI_SSID);
+        Serial.println(WIFI_PASSWORD);
+        
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        while (WiFi.status() != WL_CONNECTED) {
+          // wifi status codes: https://realglitch.com/2018/07/arduino-wifi-status-codes/
+          delay(1000);
+          Serial.print(".");
+          Serial.print(WiFi.status());
+          Serial.print(WL_CONNECTED);
+        }
+      }else{
+        Server.on("/", rootPage);
+        Serial.println("done with Server.on");  
+        config.portalTimeout = 15000;  // It will time out in 15 seconds
+        Portal.config(config);
+        Portal.begin();
+      }
+    }else{
+      deleteAllCredentials();
+    }
+  }
+  
+  /*if (Portal.begin()) {
+    Serial.println();
+    Serial.println("HTTP server:" + WiFi.localIP().toString());
+
+    // setup UDP:
+      udp.begin(UDPPort);
+  }else{
+    Serial.println("not portal.begin");
+  }
+  */
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // this handles the wifi config business:
+
+  if(!HARDCODE_SSID){
+    Portal.handleClient();
+  }
+  configUdp();
+
+  
+  sendOSCUDP(touchRead(touchPin));
+
+
+  delay(10);
+}
