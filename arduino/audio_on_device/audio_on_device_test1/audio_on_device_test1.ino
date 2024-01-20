0cbf7bda601ffa323b02da03f6ecef18ba4483ef
Don Undeen <donundeen@gmail.com>
2024-01-20 17:46:16 -0500

    new notes

--- arduino/audio_on_device/audio_on_device_test1/audio_on_device_test1.ino
+++ arduino/audio_on_device/audio_on_device_test1/audio_on_device_test1.ino
@@ -1,19 +1,19 @@
-
+// TIMING INCLUDES
 #include <AsyncTimer.h> //https://github.com/Aasim-A/AsyncTimer
-
 #include "uClock.h"
+// END TIMING INCLUDES
 
+/////////// MIDI DEFINITIONS /////////////////////
 // Solder closed jumper on bottom!
 
-// See http://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf Pg 31
+//#define VS1053_GM1_OCARINA 81
+#define VS1053_GM1_OCARINA 12 // change this for other sounds
+// See http://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf Pg 32 for more!
+
 #define VS1053_BANK_DEFAULT 0x00
 #define VS1053_BANK_DRUMS1 0x78
 #define VS1053_BANK_DRUMS2 0x7F
 #define VS1053_BANK_MELODY 0x79
-
-// See http://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf Pg 32 for more!
-//#define VS1053_GM1_OCARINA 81
-#define VS1053_GM1_OCARINA 12
 
 #define MIDI_NOTE_ON  0x90
 #define MIDI_NOTE_OFF 0x80
@@ -29,21 +29,34 @@
   // anything else? use the hardware serial1 port
   #define VS1053_MIDI Serial1
 #endif
+///  END MIDI DEFINITIONS
+/////////////////////////////////////////
 
-//notelist stuff
+
+
+///////////////////////////////
+// MUSIC PERFORMANCE VARIABLES
 int notelist[127];
 int notelistlength = 0;
 int workinglist[127];
 int workinglistlength = 0;
+
+// These might get changed at start, or during play
 int rootMidi = 0;
-int midimin = 6;
+int midimin = 6;  
 int midimax = 120;
+////// END MUSIC PERFORMANCE VARIABLES  
+///////////////////////////////////////
 
 
+
+/////////////////////////////
+// TIMING VARIABLES 
 AsyncTimer t;
-
 int bpm = 120;
 
+////////////////
+// Define the number of pulses per beat
 umodular::clock::uClockClass::PPQNResolution PPQNr = uClock.PPQN_96;
 int PPQN = 96;
 
@@ -56,8 +69,15 @@ int N16 = PPQN / 4;
 int QN3 = HN / 3;
 int HN3 = WN / 3;
 int N83 = QN / 3;
+// END TIMING VARIABLES
+////////////////////////
 
 
+
+
+
+//////////////////////////
+// TIMING FUNCTIONS
 void clocksetup(){
     // avaliable resolutions
   // [ uClock.PPQN_24, uClock.PPQN_48, uClock.PPQN_96, uClock.PPQN_384, uClock.PPQN_480, uClock.PPQN_960 ]
@@ -68,93 +88,49 @@ void clocksetup(){
   uClock.setOnClockStart(onClockStartCallback);
   uClock.setTempo(bpm);
   uClock.start();
-  t.setInterval(triggerRandNote, pulseToMS(HN));
-  t.setInterval(triggerRandNote, pulseToMS(QN3));
-  t.setInterval(switchnotelist, pulseToMS(HN * 4));
+
 }
 
 void onPPQNCallback(uint32_t tick) {
-
 } 
 
 void onClockStartCallback(){
   Serial.println("clock start");
 }
+// END TIMING FUNCTIONS
+//////////////////////////
 
-void triggerRandNote(){
-  Serial.println("trigger");
-  //int note = random(32,120);
-  int note = noteFromFloat((double)random(1000) / (double)1000, 32, 100);
-  midiMakeNote(note, 127, pulseToMS(QN));
+
+
+
+
+
+////////////////////////////
+// MIDI FUNCTIONS
+void midisetup(){
+  VS1053_MIDI.begin(31250); // MIDI uses a 'strange baud rate'
+  
+  midiSetChannelBank(0, VS1053_BANK_MELODY);
+  midiSetChannelVolume(0, 127);
+  midiSetInstrument(0, VS1053_GM1_OCARINA);
 }
 
+// Makenote: pith, velocity, duration
 void midiMakeNote(int pitch, int vel, int durationms){
+  /*
   Serial.print("MKNOTE: ");
   Serial.print(pitch);
   Serial.print(" : ");
   Serial.print(vel);
   Serial.print(" : ");
   Serial.println(durationms);
+  */
   midiNoteOn(0, pitch, vel);
    
  // int innerpitch = pitch;
   t.setTimeout([pitch, vel]() {
     midiNoteOff(0, pitch, vel);
   }, durationms);
-}
-
- 
-int notelist1[] = { 0, 3, 7, 12, 15, 19, 24, 27, 31, 36, 39, 43, 48, 51, 55, 60, 63, 67, 72, 75, 79, 84, 87, 91, 96, 99, 103, 108, 111, 115, 120, 123, 127 };
-int notelist2[] = {  1, 5, 10, 13, 17, 22, 25, 29, 34, 37, 41, 46, 49, 53, 58, 61, 65, 70, 73, 77, 82, 85, 89, 94, 97, 101, 106, 109, 113, 118, 121, 125  };
-
-void testsetup(){
-  int newlen = sizeof(notelist1)/sizeof(int);
-  setNotelist(notelist1, notelist, newlen);
-}
-
-int notelisti = 0;
-void switchnotelist(){
-  notelisti++;
-  if(notelisti % 2 == 0){
-    int newlen = sizeof(notelist1)/sizeof(int);
-    setNotelist(notelist1, notelist, newlen);
-  }else{
-    int newlen = sizeof(notelist2)/sizeof(int);
-    setNotelist(notelist2, notelist, newlen);
-  }
-}
-
-void testloop(){
-
-}
-
-void setup() {
-  delay(1000);
-  
-  Serial.begin(115200);
-
-  Serial.println("VS1053 MIDI test");
-
-  VS1053_MIDI.begin(31250); // MIDI uses a 'strange baud rate'
-  
-  midiSetChannelBank(0, VS1053_BANK_MELODY);
-  midiSetChannelVolume(0, 127);
-  midiSetInstrument(0, VS1053_GM1_OCARINA);
-
-  clocksetup();
-  testsetup();
-}
-
-void loop() {  
-  
-
-  t.handle();
-
-
-}
-
-void midiMakeNote(){
-
 }
 
 
@@ -168,7 +144,6 @@ void midiSetInstrument(uint8_t chan, uint8_t inst) {
   VS1053_MIDI.write(inst);
   delay(10);
 }
-
 
 void midiSetChannelVolume(uint8_t chan, uint8_t vol) {
   if (chan > 15) return;
@@ -207,10 +182,72 @@ void midiNoteOff(uint8_t chan, uint8_t n, uint8_t vel) {
   VS1053_MIDI.write(n);
   VS1053_MIDI.write(vel);
 }
+// END MIDI FUNCTIONS
+//////////////////////////
+
+
+/////////////////
+// TESTING FUNCTIONS
+int notelist1[] = { 0, 3, 7, 12, 15, 19, 24, 27, 31, 36, 39, 43, 48, 51, 55, 60, 63, 67, 72, 75, 79, 84, 87, 91, 96, 99, 103, 108, 111, 115, 120, 123, 127 };
+int notelist2[] = {  1, 5, 10, 13, 17, 22, 25, 29, 34, 37, 41, 46, 49, 53, 58, 61, 65, 70, 73, 77, 82, 85, 89, 94, 97, 101, 106, 109, 113, 118, 121, 125  };
+
+void testsetup(){
+  int newlen = sizeof(notelist1)/sizeof(int);
+  setNotelist(notelist1, notelist, newlen);
+  /// This part just for testing
+  t.setInterval(triggerRandNote, pulseToMS(HN));
+  t.setInterval(triggerRandNote, pulseToMS(QN3));
+  t.setInterval(switchnotelist, pulseToMS(HN * 4));
+}
+
+void triggerRandNote(){
+  Serial.println("trigger");
+  //int note = random(32,120);
+  int note = noteFromFloat((double)random(1000) / (double)1000, 32, 100);
+  midiMakeNote(note, 127, pulseToMS(QN));
+}
+
+int notelisti = 0;
+void switchnotelist(){
+  notelisti++;
+  if(notelisti % 2 == 0){
+    int newlen = sizeof(notelist1)/sizeof(int);
+    setNotelist(notelist1, notelist, newlen);
+  }else{
+    int newlen = sizeof(notelist2)/sizeof(int);
+    setNotelist(notelist2, notelist, newlen);
+  }
+}
+// END TEST FUNCTIONS
+/////////////////////////
 
 
 
-// Music Theory Things
+/////////////////////////////
+// SETUP AND LOOP FUNCTIONS
+// calls other setup functions
+void setup() {
+  delay(1000);
+  
+  Serial.begin(115200);
+
+  midisetup();
+  clocksetup();
+  testsetup();
+}
+
+void loop() {  
+  // everything else should be handled as async calls
+  t.handle();
+}
+// END SETUP AND LOOP FUNCTIONS
+/////////////////////////////////
+
+
+
+//////////////////////////////////
+// MUSIC PERFORMANCE FUNCTIONS
+
 void setNotelist(int* newlist, int* curlist, int size){
   notelistlength = size;
   memcpy(curlist, newlist, sizeof(newlist[0])*size);
@@ -219,7 +256,6 @@ void setNotelist(int* newlist, int* curlist, int size){
 void setRoot(int root){
   rootMidi = root;
 }
-
 
 int noteFromFloat(double value, int min, int max){
   makeworkinglist(min, max);
@@ -253,15 +289,6 @@ int getRootedBestNoteFromFloat(int value, int min, int max){
 		return false;
 	}
 	return note;
-}
-
-int indexOf(int value, int searcharray[], int length){
-  for (int i = 0; i < length; i++) {
-    if (searcharray[i] == value) {
-      return i;
-    }
-  }
-  return -1;
 }
 
 int moveMinMax(int root, int minmax){
@@ -307,7 +334,21 @@ void makeworkinglist(int minval, int maxval){
   }
   workinglistlength = wi + 1;
 }
+// END MUSIC PERFORMANCE FUNCTIONS
+/////////////////////////////////
 
+
+
+////////////////////
+// HELPER FUNCTIONS
+int indexOf(int value, int searcharray[], int length){
+  for (int i = 0; i < length; i++) {
+    if (searcharray[i] == value) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 // get number of milliseconds for some number of pulses
 int pulseToMS(int pulses){
