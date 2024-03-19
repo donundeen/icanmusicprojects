26fae472b9dfbf52810c31bfc1da9cdd4d81ac69
Don Undeen <donundeen@gmail.com>
2024-03-18 20:36:59 -0400

    new notes

--- server/synthplayer.node.js
+++ server/synthplayer.node.js
@@ -2,14 +2,20 @@ var JZZ = require('jzz');
 require('jzz-synth-fluid')(JZZ);
 
 let args = ["a", "coreaudio"];
+let program = 22;
+
+///Users/donundeen/Downloads/MuseScore_General.sf2
+let soundfont = '/Users/donundeen/Documents/htdocs/icanmusicprojects/server/soundfonts/GeneralUserGS/GeneralUserGS.sf2'
+//let soundfont = "/Users/donundeen/Downloads/MuseScore_General.sf2";
 
 console.log("gonna play");
 
 JZZ.synth.Fluid({ path: '/opt/homebrew/bin/fluidsynth', 
-                sf: '/Users/donundeen/Documents/htdocs/icanmusicprojects/server/soundfonts/GeneralUserGS/GeneralUserGS.sf2',
+                sf: soundfont,
                 args: args })
-   .noteOn(0, 'C5', 127)
-   .wait(500).noteOn(0, 'E5', 127)
-   .wait(500).noteOn(0, 'G5', 127)
-   .wait(500).noteOff(0, 'C5').noteOff(0, 'E5').noteOff(0, 'G5')
-   .close();
+    .program(0,program)
+    .noteOn(0, 'C5', 127)
+    .wait(500).noteOn(0, 'E5', 127)
+    .wait(500).noteOn(0, 'G5', 127)
+    .wait(500).noteOff(0, 'C5').noteOff(0, 'E5').noteOff(0, 'G5')
+    .close();
